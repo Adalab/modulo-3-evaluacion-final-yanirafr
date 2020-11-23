fee9ccaa0783339507f14252bbcdfc1d57c08327
Yanira <yanirafr@gmail.com>
2020-11-23 16:05:11 +0100

    add fetch function and useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "../stylesheet/App.scss";
+import { useState, useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
+import { getDataFromApi } from "../services/api";
 
 const App = () => {
-  return <>Hola mundo</>;
+  // Life cicle
+  useEffect(() => {
+    getDataFromApi().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
+  // Return
+  return <div className="">Holi</div>;
 };
 
 export default App;
