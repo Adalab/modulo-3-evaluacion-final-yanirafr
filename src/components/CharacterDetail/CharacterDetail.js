e9d2fd0a0387bb3ab3311f381ee3b842da740c8f
Yanira <yanirafr@gmail.com>
2020-11-24 13:50:02 +0100

    add character detail css styles

--- src/components/CharacterDetail/CharacterDetail.js
+++ src/components/CharacterDetail/CharacterDetail.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
 import "./CharacterDetail.scss";
 import PropTypes from "prop-types";
+import portal from "../../images/home-portal.png";
+import human from "../../images/human.png";
+import alien from "../../images/alien.png";
+import alive from "../../images/alive.png";
+import dead from "../../images/dead.png";
+import unknown from "../../images/unknown.png";
 
 const CharacterDetail = (props) => {
   return (
     <>
-      <Link to="/" className="home-link">
-        Home
+      <Link to="/" className="home-link" title="Home portal">
+        <img src={portal} alt="Home-portal" className="home-link__icon" />
+        <p className="home-link__text">Home portal</p>
       </Link>
       <article className="character-detail" id={props.id}>
         <img
@@ -15,7 +22,29 @@ const CharacterDetail = (props) => {
           title={props.name}
           className="character-detail__image"
         />
-        <h2 className="character-detail__name">{props.name}</h2>
+        <div className="character-detail__name">
+          <h2 className="character-detail__name__text">{props.name}</h2>
+          <div className="character__detail__icons">
+            <img
+              src={props.species === "human" ? human : alien}
+              alt={props.species}
+              className="character-detail__icon"
+              title={"Species: " + props.species}
+            />
+            <img
+              src={
+                props.status === "Alive"
+                  ? alive
+                  : props.status === "Dead"
+                  ? dead
+                  : unknown
+              }
+              alt={props.status}
+              className="character-detail__icon"
+              title={"Status: " + props.status}
+            />
+          </div>
+        </div>
         <ul className="character-detail__details">
           <li className="character-detail__details__item">
             Status: {props.status}
